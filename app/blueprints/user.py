234b0b711428e6cf8a9a90aba97e21c0e4b5ddb3
AbdulHadi-Programmer <abdulhadistmstudent@gmail.com>
2024-01-07 06:58:30 -0800

    Add new folders and files and also solve some problems

--- app/blueprints/user.py
+++ app/blueprints/user.py
@@ -8,7 +8,7 @@ def manage_users():
         return "<h1>Create New User!</h1>"
     else:
         # return "<h1>Show All Users:</h1>"
-        return render_template("user.html")
+        return render_template("user/index.html")
 
 @bp.route('/user/<int:id>', methods=["DELETE", "PUT"])
 def user_id(id):
@@ -16,3 +16,8 @@ def user_id(id):
         return "<h1>Delete User!</h1>"
     elif request.method == "PUT":
         return "<h1>Update User!</h1>"
+    
+@bp.route('/user/<int:id>')
+def about(id):
+    # return f'<h1>Welcome {id}</h1>'
+    return render_template("user/show.html", id=id)
